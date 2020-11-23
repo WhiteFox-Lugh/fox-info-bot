690fa4790075c3bd3e0a6776dbd1d8c677fcf7c3
WhiteFox-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-11-24 03:09:54 +0900

    feat : fox status v1.0

--- fox-status.go
+++ fox-status.go
@@ -5,39 +5,60 @@ import (
 	"github.com/ChimeraCoder/anaconda"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
 // FoxStatus : get today's status
 func FoxStatus(api *anaconda.TwitterApi) {
 	const screenName = "Arthur_Lugh"
+	const myID = "3145003784"
 	const baseURL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 	const dateFormat = "2006-01-02"
 	const parseDateFormat = "Mon Jan 2 15:04:05 -0700 2006"
+	const getTweetCount = "200"
+	const durationDay = 24 * 60 * 60
+	jst, _ := time.LoadLocation("Asia/Tokyo")
 
 	values := url.Values{}
 	values.Add("screen_name", screenName)
+	values.Add("count", getTweetCount)
 	response, err := api.GetUserTimeline(values)
 	if err != nil {
 		panic(err)
 	}
 
-	// get yesterday timestamp (-1h)
+	// get timestamp
 	time.Local = time.FixedZone("JST", 9*60*60)
 	day := time.Now()
-	day = day.Add(-time.Duration(1) * time.Hour)
-	fmt.Println(day)
+	timeStr := day.Format(dateFormat)
+	currentDayObj, _ := time.ParseInLocation(dateFormat, timeStr, jst)
+	fmt.Println(currentDayObj)
 
 	count := 0
-	jst, _ := time.LoadLocation("Asia/Tokyo")
+
 	// check timestamp
-	for i := 0; i < 5; i++ {
+	for i := 0; i < len(response); i++ {
 		timestamp := response[i].CreatedAt
-		fmt.Println(timestamp)
-		timeObj, _ := time.ParseInLocation(parseDateFormat, timestamp, jst)
-		fmt.Println(timeObj)
+		idStr := response[i].User.IdStr
+		source := response[i].Source
+
+		if strings.Contains(source, "狐日和") || idStr != myID {
+			continue
+		}
+
+		timeObj, _ := time.Parse(parseDateFormat, timestamp)
+		timeObj = timeObj.In(jst)
+
+		// current time - tweet time
+		duration := currentDayObj.Sub(timeObj)
+		durationSec := int(duration.Seconds())
+
+		if 0 <= durationSec && durationSec < durationDay {
+			count++
+		}
 	}
-	tweetText := day.Format(dateFormat) + "\nTweet Count:" + strconv.Itoa(count)
+	tweetText := "(っ ॑꒳ ॑c).+(" + day.Format(dateFormat) + " Report)\n前日ツイート数（bot 除外）:" + strconv.Itoa(count)
 	fmt.Println(tweetText)
 	return
 }
