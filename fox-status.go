273c4ebb298d057ec82bd6dcb198f9dd17558e4c
WhiteFox-Lugh <38004439+WhiteFox-Lugh@users.noreply.github.com>
2020-11-27 02:38:08 +0900

    feat : github contribution num

--- fox-status.go
+++ fox-status.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/ChimeraCoder/anaconda"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
@@ -10,6 +14,14 @@ import (
 
 const dateFormat = "2006-01-02"
 
+// EventData : github の Event のデータをもつ
+type EventData struct {
+	ID        string `json:"id"`
+	Type      string `json:"type"`
+	CreatedAt string `json:"created_at"`
+}
+
+// countTweetNum : 一日のツイート数を取得
 func countTweetNum(api *anaconda.TwitterApi) (count int, day time.Time) {
 	const screenName = "Arthur_Lugh"
 	const myID = "3145003784"
@@ -45,6 +57,7 @@ func countTweetNum(api *anaconda.TwitterApi) (count int, day time.Time) {
 			continue
 		}
 
+		// parse して jst に変換
 		timeObj, _ := time.Parse(parseDateFormat, timestamp)
 		timeObj = timeObj.In(jst)
 
@@ -59,14 +72,77 @@ func countTweetNum(api *anaconda.TwitterApi) (count int, day time.Time) {
 	return
 }
 
+// getGithubCommitcount : 1日の GitHub event 数を取得
+func getGithubCommitCount(api *anaconda.TwitterApi) (commitCount int) {
+	const endpoint = "https://api.github.com/users/WhiteFox-Lugh/events"
+	const githubDateFormat = "2006-01-02T15:04:05Z"
+	const maxPage = 1
+	const durationDay = 24 * 60 * 60
+	var jsonData []EventData
+
+	jst, _ := time.LoadLocation("Asia/Tokyo")
+
+	// get timestamp (日付の境界)
+	time.Local = time.FixedZone("JST", 9*60*60)
+	day := time.Now()
+	timeStr := day.Format(dateFormat)
+	currentDayObj, _ := time.ParseInLocation(dateFormat, timeStr, jst)
+	fmt.Println(currentDayObj)
+
+	for i := 1; i <= maxPage; i++ {
+		// endpoint 生成
+		values := url.Values{}
+		values.Add("page", strconv.Itoa(i))
+
+		//リクエストの送信
+		request, _ := http.NewRequest("GET", endpoint+"?"+values.Encode(), nil)
+		response, err := http.DefaultClient.Do(request)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		defer response.Body.Close()
+
+		// parse json
+		byteArray, err := ioutil.ReadAll(response.Body)
+		if err := json.Unmarshal(byteArray, &jsonData); err != nil {
+			panic(err)
+		}
+
+		for j := 0; j < len(jsonData); j++ {
+			// created_at の文字列を時刻としてパースし、JST に変換
+			createdAt := jsonData[j].CreatedAt
+			timeObj, _ := time.Parse(githubDateFormat, createdAt)
+			timeObj = timeObj.In(jst)
+			fmt.Println(jsonData[j].CreatedAt)
+			fmt.Println(timeObj)
+
+			// current time - tweet time
+			duration := currentDayObj.Sub(timeObj)
+			durationSec := int(duration.Seconds())
+			if 0 <= durationSec && durationSec < durationDay {
+				commitCount++
+			} else {
+				goto Checked
+			}
+		}
+	}
+
+Checked:
+	return
+}
+
 // FoxStatus : get today's status
 func FoxStatus(api *anaconda.TwitterApi) {
 
 	// 一日のツイート数を取得
 	count, day := countTweetNum(api)
+	eventNum := getGithubCommitCount(api)
 	tweetHeader := "(っ ॑꒳ ॑c).+(" + day.Format(dateFormat) + " 0:00 Report)"
 	tweetCountText := "前日ツイート数（bot 除外）:" + strconv.Itoa(count)
-	tweetText := tweetHeader + "\n" + tweetCountText
+	tweetCommitText := "GitHub Event 数 : " + strconv.Itoa(eventNum)
+	tweetText := tweetHeader + "\n" + tweetCountText + "\n" + tweetCommitText
+	fmt.Println(tweetText)
 	_, postErr := api.PostTweet(tweetText+"\n(bot)", nil)
 
 	if postErr != nil {
